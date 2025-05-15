2bb483fe8454f348ef0a6271cac26d5758a29617
김찬기 (Chanki Kim) <44834561+cksrlcks@users.noreply.github.com>
2025-05-15 23:34:46 +0900

    Feat/48/mypage info (#58)
    * style: adjust mypage layout and navigation styles
    
    * feat: add user profile edit action and turn on delete user option
    
    * feat: add user profile page

--- src/app/my/layout.tsx
+++ src/app/my/layout.tsx
@@ -1,13 +1,23 @@
 import { PropsWithChildren } from "react";
 import MypageNav from "@/components/MypageNav";
+import { Separator } from "@/components/ui/separator";
 
 export default function MypageLayout({ children }: PropsWithChildren) {
   return (
-    <div className="flex min-h-[400px] gap-4">
-      <div className="w-[200px]">
-        <MypageNav />
+    <div className="min-h-[400px]">
+      <header className="pt-4 md:pt-0">
+        <h2 className="mb-1 text-lg font-semibold md:text-xl">마이페이지</h2>
+        <p className="text-muted-foreground text-xs md:text-sm">
+          회원정보 수정 및 나의 활동을 확인할 수 있어요
+        </p>
+      </header>
+      <Separator className="bg-muted my-4 md:my-8" />
+      <div className="flex flex-col gap-8 md:flex-row md:gap-16">
+        <div className="md:w-[200px]">
+          <MypageNav />
+        </div>
+        <div className="flex-1">{children}</div>
       </div>
-      <div className="flex-1">{children}</div>
     </div>
   );
 }
---
+++ src/features/my/components/Profile.tsx
@@ -0,0 +1,121 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { User } from "better-auth";
+import { toast } from "sonner";
+import { editUserProfileAction } from "@/actions/auth";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import {
+  UserProfileEditForm,
+  userProfileEditSchema,
+} from "@/features/auth/type";
+import { authClient } from "@/lib/auth-client";
+
+type ProfileProps = {
+  user: Partial<User>;
+};
+
+export function Profile({ user }: ProfileProps) {
+  const router = useRouter();
+  const form = useForm<UserProfileEditForm>({
+    resolver: zodResolver(userProfileEditSchema),
+    defaultValues: {
+      name: user.name,
+    },
+  });
+
+  const handleSubmit = form.handleSubmit(async (data) => {
+    const response = await editUserProfileAction(data);
+    if (response.success) {
+      toast.success(response.message);
+      router.refresh();
+    } else {
+      toast.error(response.message);
+    }
+  });
+
+  const handleUserDelete = async () => {
+    await authClient.deleteUser();
+    window.location.href = "/";
+  };
+
+  return (
+    <>
+      <header>
+        <h2 className="mb-1 font-semibold">회원정보 수정</h2>
+        <p className="text-muted-foreground text-xs md:text-sm">
+          가입된 정보를 수정 하실 수 있어요
+        </p>
+      </header>
+      <Separator className="bg-muted my-6" />
+      <Form {...form}>
+        <form onSubmit={handleSubmit} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>이름</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex justify-end gap-1">
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button type="button" variant="outline">
+                  회원탈퇴
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>정말 탈퇴하시겠습니까?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    회원탈퇴를 하면 다시 복구가 불가능합니다.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>취소</AlertDialogCancel>
+                  <AlertDialogAction onClick={handleUserDelete}>
+                    탈퇴
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+
+            <Button type="submit" disabled={form.formState.isSubmitting}>
+              수정
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </>
+  );
+}

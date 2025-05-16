590f427f585beff564b4199a72329363c887bd81
chanki.kim <chanki.kim89@gmail.com>
2025-05-16 21:59:11 +0900

    feat: add loading component for mypage

---
+++ src/app/my/loading.tsx
@@ -0,0 +1,9 @@
+import Spinner from "@/components/Spinner";
+
+export default function Loading() {
+  return (
+    <div className="flex w-full justify-center py-10">
+      <Spinner />
+    </div>
+  );
+}

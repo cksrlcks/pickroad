8f49ab02d22b133f080313f3fd07210fee42e21f
chanki.kim <chanki.kim89@gmail.com>
2025-05-17 11:39:12 +0900

    feat: add  bookmark type and export schema

--- src/features/bookmark/type.ts
+++ src/features/bookmark/type.ts
@@ -4,8 +4,6 @@ import { bookmarks } from "@/db/schema/bookmarks";
 import { RoadmapCompact } from "../roadmap/type";
 
 export const bookmarkSchema = createSelectSchema(bookmarks);
-export type Bookmark = z.infer<typeof bookmarkSchema>;
-
-export type ActivityBookmark = Bookmark & {
+export type Bookmark = z.infer<typeof bookmarkSchema> & {
   roadmap: Partial<RoadmapCompact> | null;
 };

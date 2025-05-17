7abae373213a9216c82fcfa42d0521dd660a1ece
chanki.kim <chanki.kim89@gmail.com>
2025-05-17 15:59:51 +0900

    refactor: Refactor: remove login redirect logic from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,6 @@ import { getSessionCookie } from "better-auth/cookies";
 
 export async function middleware(request: NextRequest) {
   const sessionCookie = getSessionCookie(request);
-  const pathname = request.nextUrl.pathname;
-
-  if (pathname.startsWith("/login")) {
-    if (sessionCookie) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
-    return NextResponse.next();
-  }
 
   if (!sessionCookie) {
     return NextResponse.redirect(new URL("/login", request.url));

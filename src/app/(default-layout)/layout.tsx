d230a55c0c2092977882bb33c28c5006e609417a
김찬기 (Chanki Kim) <44834561+cksrlcks@users.noreply.github.com>
2025-06-18 16:39:17 +0900

    Revert "Fix/118/static page bailout (#119)" (#121)
    This reverts commit e0288486c68f416ae2e48f26b25a3a7a564ac98a.

--- src/app/(default-layout)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { PropsWithChildren } from "react";
-import { DefaultLayout } from "@/components/Layout";
-
-export default function Layout({ children }: PropsWithChildren) {
-  return <DefaultLayout>{children}</DefaultLayout>;
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
+import { Suspense } from "react";
 import { Metadata } from "next";
 import { Roboto_Serif } from "next/font/google";
-import { Toaster } from "sonner";
+import FilterProvider from "@/components/FilterProvider";
 import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Inner from "@/components/Inner";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
 const robotoSerif = Roboto_Serif({
@@ -60,16 +64,23 @@ export default function RootLayout({
         ></script>
       </head>
       <body className="group antialiased">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-          <Footer />
-          <Toaster />
-        </ThemeProvider>
+        <Suspense>
+          <FilterProvider basePath="/">
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <Header />
+              <Inner className="pb-10">
+                <main>{children}</main>
+              </Inner>
+              <Footer />
+              <Toaster />
+            </ThemeProvider>
+          </FilterProvider>
+        </Suspense>
       </body>
     </html>
   );

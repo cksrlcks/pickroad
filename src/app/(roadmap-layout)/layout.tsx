0f4c54edd3089d17ffdb773e7e680920e09c191b
김찬기 (Chanki Kim) <44834561+cksrlcks@users.noreply.github.com>
2025-06-18 16:37:51 +0900

    Revert "Fix/118/static page bailout (#119)"
    This reverts commit e0288486c68f416ae2e48f26b25a3a7a564ac98a.

--- src/app/(roadmap-layout)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { PropsWithChildren } from "react";
-import { RoadmapLayout } from "@/components/Layout";
-
-export default function Layout({ children }: PropsWithChildren) {
-  return <RoadmapLayout>{children}</RoadmapLayout>;
-}
